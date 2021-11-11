e8d2c141d20b2b068eac987eee2bb4bf768e3754
rikitorres1 <94098913+rikitorres1@users.noreply.github.com>
2021-11-11 01:51:42 -0400

    Add files via upload

---
+++ ev 2 de programacion.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace ev_2_de_programacion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             string cadena ="";
+            int edad = 0;
+            double altura = 0.0;
+            
+            
+            Console.WriteLine("buenas tardes y mucho gusto, por favor ingrese su edad:");
+            cadena = Console.ReadLine();
+            edad = Convert.ToInt32(cadena);
+            
+            
+            if(edad >= 18)
+            {
+                Console.WriteLine("lo sentimos, estas instalaciones son para menores de 18 años.");
+            } 
+           else
+            {
+                Console.WriteLine("Bienvenido a mundo aventura");
+                Console.WriteLine("Me podria decir su altura para indicarle a donde puede ir?");
+                cadena = Console.ReadLine();
+                altura = Convert.ToDouble(cadena);
+
+                if(altura < 1.6)
+                {
+                    Console.WriteLine("solo puedes ir a el gusanito y al carrusel por nuestras medidas de seguridad y prevencion");
+                  } 
+                  else
+                  {   
+                      Console.WriteLine("tienes disponibles todas las atracciones del parque menos el carrucel que es para los mas chiquitos,");
+                    }   
+                    Console.WriteLine("Disfruten su paseo por nuestras instalaciones, tengan un gran de en mundo aventura!");
+                    {
+            } 
+                        
+                
+             
+            }
+           
+        }
+    }
+}
